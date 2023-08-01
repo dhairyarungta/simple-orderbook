99c7bb317ecc17b4d26bd29c02277c9fcda81637
dhairyarungta <97079960+dhairyarungta@users.noreply.github.com>
2023-08-02 02:32:21 +0800

    c code

--- src/hftlob.h
+++ src/hftlob.h
@@ -21,5 +21,99 @@ typedef struct Order
 
 typedef struct Limit
 {
+    double limitPrice ;
+    double size ;
+    double totalVolume;
+    int orderCount ;
+    struct Limit*parent ;
+    struct Limit*leftChild;
+    struct Limit* rightChild ;
+    struct Order* headOrder;
+    struct Order* tailOrder;
 
-}
+}Limit ;
+
+typedef struct QueueItem
+{
+    Limit *limit ;
+    struct QueueItem *previous ;
+}QueueItem;
+
+typedef struct Queue
+{
+    int size ;
+    QueueItem *head ;
+    QueueItem *tail;
+
+}Queue;
+
+
+void initOrder(Order*order);
+
+void initQueueItem(QueueItem* item)
+
+void initLimit(Limit *limit);
+
+void initQueue(Queue*q);
+
+
+
+void pushToQueue(Queue*q, Limit*limit);
+
+Limit* popFromQueue(Queue*q);
+
+int queueIsEmpty(Queue *q);
+
+
+
+int pushOrder(Limit*limit, Order*new_order);
+
+Order* popOrder(Limit*limit);
+
+int removeOrder(Order* order);
+
+
+
+Limit* createRoot(void);
+
+int addNewLimit(Limit*root, Limit* limit);
+
+void replaceLimitInParent (Limit* limit, Limit* newLimit);
+
+int removeLimit(Limit* limit);
+
+
+
+void balanceBranch(Limit*limit);
+
+void rotateLeftLeft(Limit* limit);
+
+void rotateLeftRight(Limit *limit);
+
+void rotateRightRight(Limit* limit);
+
+void rotateRightLeft(Limit*limit);
+
+
+int limitExists(Limit*root, Limit*limit);
+
+int limitIsRoot(Limit* limit);
+
+int hasGrandpa(Limit* limit);
+
+int getGrandpa(Limit* limit);
+
+int getMinimumLimit(Limit* limit);
+
+int getMaximumLimit(Limit* limit);
+
+int getHeight(Limit* limit);
+
+int getBalanceFactor (Limit* limit);
+
+void copyLimit(Limit*ptr_src, Limit*ptr_target);
+
+void RunAllTests();
+
+
+#endif
---
+++ src/orders.c
@@ -0,0 +1,7 @@
+
+#include <math.h>
+#include<assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "hftlob.h"
+
